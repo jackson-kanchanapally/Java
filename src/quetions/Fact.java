9fc3559791805fffcc5b73236e33d49f7ed045f0
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-07-19 23:42:40 +0530

    fact.java

--- src/quetions/Fact.java
+++ src/quetions/Fact.java
@@ -1,5 +1,11 @@
+import java.util.*;
 public class Fact {
+
     public static void main(String[] args) {
+        int a;
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the value :");
         
+
     }
 }
