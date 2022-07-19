636f1578d395604ff8f41ee8aad6f954252ac521
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-07-20 00:05:56 +0530

    fact.java

--- src/quetions/Fact.java
+++ src/quetions/Fact.java
@@ -5,8 +5,7 @@ public class Fact {
         int val,fact=1;
         Scanner in=new Scanner(System.in);
         System.out.println("Enter the value :");
-        val=in.nextInt();
-        for(int i=1;i<=val;i++)
+        val=in.nextInt(); for(int i=1;i<=val;i++)
         {
             fact=fact*i;
         }
