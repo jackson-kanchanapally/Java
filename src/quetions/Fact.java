09ce17d39f8ddd1e71333c1ead9a1672d5d2d136
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-07-20 00:04:04 +0530

    fact.java

--- src/quetions/Fact.java
+++ src/quetions/Fact.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Fact {
 
     public static void main(String[] args) {
-        int a,fact=1;
+        int val,fact=1;
         Scanner in=new Scanner(System.in);
         System.out.println("Enter the value :");
         a=in.nextInt();
