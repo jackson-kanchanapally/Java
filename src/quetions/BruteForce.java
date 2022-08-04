8af364fd0bea79d9fb25510a26e6073bd6671eba
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-08-04 22:28:41 +0530

    updated brute force

--- src/quetions/BruteForce.java
+++ src/quetions/BruteForce.java
@@ -5,7 +5,7 @@ public class BruteForce {
         Scanner in=new Scanner(System.in);
         System.out.print("Enter your four digit password : ");
         int a=in.nextInt();
-        for(int i=1111;i<10000;i++)
+        for(int i=0000;i<10000;i++)
         {
             if(i==a)
             {
