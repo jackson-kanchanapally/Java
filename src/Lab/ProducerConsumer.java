a9252be8a48f53ebf7ebf762670ae8b1039961b9
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-08-17 23:14:53 +0530

    producer consumer

--- src/Lab/ProducerConsumer.java
+++ src/Lab/ProducerConsumer.java
@@ -1,80 +1,77 @@
 import java.util.*;
 
-class ItemQueue {
-	int item;
-	boolean valueSet = false;
+class Q {
+	private int i;
+	private boolean available = false; // Assume as Buffer which is empty
 
-	synchronized int getItem()
-
+	public synchronized void consume() // Multiple threads cannot access the block sametime.
 	{
-		while (!valueSet)
+		while (!available) {
 			try {
 				wait();
 			} catch (InterruptedException e) {
-				System.out.println("InterruptedException caught");
 			}
-		System.out.println("Consummed:" + item);
-		valueSet = false;
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			System.out.println("InterruptedException caught");
 		}
-		notify();
-		return item;
-	}
-
-	synchronized void putItem(int item) {
-		while (valueSet)
-			try {
-				wait();
-			} catch (InterruptedException e) {
-				System.out.println("InterruptedException caught");
-			}
-		this.item = item;
-		valueSet = true;
-		System.out.println("Produced: " + item);
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			System.out.println("InterruptedException caught");
-		}
-		notify();
-	}
-}
-
-class Producer implements Runnable{
-	ItemQueue itemQueue;
-	Producer(ItemQueue itemQueue){
-		this.itemQueue = itemQueue;
-		new Thread(this, "Producer").start();
-	}
-	public void run() {
-		int i = 0;
-		while(true) {
-			itemQueue.putItem(i++);
-		}
-	}
-}
-class Consumer implements Runnable{
-	
-	ItemQueue itemQueue;
-	Consumer(ItemQueue itemQueue){
-		this.itemQueue = itemQueue;
-		new Thread(this, "Consumer").start();
-	}
-	public void run() {
-		while(true) {
-			itemQueue.getItem();
-		}
-	}
-}
-
-class ProducerConsumer{
-	public static void main(String args[]) {
-		ItemQueue itemQueue = new ItemQueue();
-		new Producer(itemQueue);
-		new Consumer(itemQueue);
+		available = false;
+		System.out.println("Data consumed "+i);
+		notifyAll(); // This method is used to wakeup the threads which are waiting.
 		
+	}
+
+	public synchronized void produce(int value) {
+		while (available) {
+			try {
+				wait(0);
+			} catch (InterruptedException e) {
+			}
+		}
+		i = value;
+		available = true;
+		System.out.println("Data produced "+i);
+		notifyAll();
+	}
+
+}
+/*
+ * 1.Producer only generate the element into buffer or box when it is EMPTY.
+ * 2.Consumer take the element from the buffer when it contains an element.
+ * 3.Produce and consumer can't work at same time.
+ */
+
+class consumer extends Thread {
+	private Q q;
+
+	public consumer(Q c) {
+		this.q = c;
+	}
+
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			q.consume();
+		}
+	}
+}
+
+class Producer extends Thread {
+	private Q q;
+
+	public Producer(Q c) {
+		this.q = c;
+
+	}
+
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			q.produce(i);
+		}
+	}
+}
+public class ProducerConsumer{
+	public static void main(String[] args) {
+		Q q=new Q();
+		Producer p= new Producer(q);
+		consumer c= new consumer(q);
+		p.start();
+		c.start();
 	}
 }
